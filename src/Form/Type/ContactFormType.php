9f6237f35a0969254fdb81f1f33cc2a116a91a83
Pierre MINIGGIO <=>
2020-01-30 11:21:04 +0100

    validator

--- src/Form/Type/ContactFormType.php
+++ src/Form/Type/ContactFormType.php
@@ -9,7 +9,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactFormType extends AbstractType
 {
@@ -23,8 +25,18 @@ class ContactFormType extends AbstractType
                     new Length(['min' => 2, 'max' => 75])
                 ]
             ])
-            ->add('email', EmailType::class)
-            ->add('message', TextareaType::class)
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Email()
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 20, 'max' => 2500])
+                ]
+            ])
             ->add('send', SubmitType::class, ['label' => 'Envoyer'])
         ;
     }
